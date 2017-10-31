2d688b780e2a1816437274a1f0e970ccbaad38ed
Ryan Steinmetz <zi@FreeBSD.org>
2017-10-31 10:29:37 -0400

    Add HTTPS support to SourceForge site handler

--- Portscout/SiteHandler/SourceForge.pm
+++ Portscout/SiteHandler/SourceForge.pm
@@ -85,7 +85,7 @@ sub CanHandle
 
 	my ($url) = @_;
 
-	return ($url =~ /^http:\/\/[^\/]*?\.sourceforge\.net\/project\//);
+	return ($url =~ /^https?:\/\/[^\/]*?\.sourceforge\.net\/project\//);
 }
 
 
