47123eecab4d1866553f45c1175b6c83c39d6a14
Ryan Steinmetz <zi@FreeBSD.org>
2022-01-30 16:12:23 -0500

    Skip if the site is empty

--- Portscout/DataSrc/Ports.pm
+++ Portscout/DataSrc/Ports.pm
@@ -454,6 +454,8 @@ sub BuildPort
 		$site =~ s/\/+$/\//;
 		$site =~ s/:[A-Za-z0-9][A-Za-z0-9\,]*$//g; # site group spec.
 
+		next if ($site eq "");
+
 		$site = URI->new($site)->canonical;
 		next if (length $site->host == 0);
 
